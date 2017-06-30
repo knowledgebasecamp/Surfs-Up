1c24f2b96b49800c3aeb3ecf27d8a253129ec554
KnowledgeBasecamp <knowledgebasecamp1@gmail.com>
2017-06-30 13:39:02 -0400

    Setup Models

---
+++ surfs-up-website/api/models/Product.js
@@ -0,0 +1,30 @@
+/**
+ * Product.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title:{
+      type: 'string'
+    },
+    description:{
+      type: 'string'
+    },
+    price:{
+      type: 'float'
+    },
+    salePrice:{
+      type: 'float'
+    },
+    thumbNailImg:{
+      type: 'string'
+    },
+    allImages:{
+      type: 'array'
+    }
+  }
+};
